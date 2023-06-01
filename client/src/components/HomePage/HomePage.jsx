ea398086bb04fa0f569b0b365877c91ddd8a02d9
Buszek <ak5224208@gmail.com>
2023-06-01 23:30:42 +0200

    added appearance of main menu on phones

--- client/src/components/HomePage/HomePage.jsx
+++ client/src/components/HomePage/HomePage.jsx
@@ -4,9 +4,12 @@ import './HomePage.css'
 const HomePage = () => {
 	return (
 		<div className="relative w-full h-screen flex justify-around items-center">
-			<div className="w-1/2 xl:w-1/4 text-white">
-				<h1 className="text-6xl font-semibold text-shadow shadow-black">Rozwiń swój biznes</h1>
-				<p className="mt-5 text-lg">Zbuduj własną firmę od podstaw. Zainteresowany?</p>
+			<div className="w-[90%] md:w-2/3 xl:w-1/4 text-white">
+				<h1 className="text-5xl md:text-6xl font-semibold text-shadow shadow-black">Rozwiń swój biznes</h1>
+				<p className="mt-5 md:text-xl">
+					Zbuduj własną firmę od podstaw.
+					<br /> Zainteresowany?
+				</p>
 
 				<div className="w-full flex gap-5 mb-5 mt-8">
 					<Link to={'/auth'} className="w-full">
@@ -21,7 +24,7 @@ const HomePage = () => {
 					darmo! Nasz zaawansowany model symulacji ekonomicznej zapewnia najlepsze wrażenia z gry.
 				</p>
 			</div>
-			<div></div>
+			<div className="hidden md:flex"></div>
 			<div className="absolute top-0 left-0 w-full h-full bg-black opacity-40 -z-10"></div>
 			<img src="./Background.jpg" alt="" className="absolute h-full object-cover top-0 left-0 w-full -z-20" />
 		</div>
