4298dfa0f254964ecb08a923774c7b9619b9810f
Buszek <ak5224208@gmail.com>
2023-06-01 19:25:57 +0200

    test deploy

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
+	res.setHeader('Access-Control-Allow-Origin', process.env.WEB_LINK)
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 	res.setHeader('cache-control', 'no-cache')
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 app.use(
 	cors({
 		credentials: true,
-		origin: 'http://localhost:5173',
+		origin: process.env.WEB_LINK,
 	})
 )
 
