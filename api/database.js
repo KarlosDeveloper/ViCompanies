01132a9345152b5ab98f35c3c79425bca7fccadd
Buszek <ak5224208@gmail.com>
2023-06-01 19:46:57 +0200

    test back-end deploy

--- api/database.js
+++ api/database.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose')
-require('dotenv').config()
 
 // Connect from Database
 module.exports = mongoose.connect(
--- api/index.js
+++ api/index.js
@@ -11,7 +11,8 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', process.env.WEB_LINK)
+	// res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173')
+	res.setHeader('Access-Control-Allow-Origin', 'https://vicompanies.netlify.app/')
 	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 	res.setHeader('cache-control', 'no-cache')
@@ -20,7 +21,8 @@ app.use((req, res, next) => {
 app.use(
 	cors({
 		credentials: true,
-		origin: process.env.WEB_LINK,
+		// origin: 'http://localhost:5173',
+		origin: 'https://vicompanies.netlify.app/',
 	})
 )
 
